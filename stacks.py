8fb3900d43e745225c82b0b3ed01ea87cf4f091c
eshe-aziz <eshehaledhaziz@gmail.com>
2024-03-31 18:57:16 +0300

    Added a file on stacks

---
+++ stacks.py
@@ -0,0 +1,11 @@
+#creating a stack
+stack = []
+stack.append(100)
+stack.append(200)
+stack.append(300)
+
+print(stack)
+
+#accessing item in a stack
+my_stack = ['Kenya', 'Uganda', 'Rwanda', 'Zambia', 'Zimbabwe']
+print(my_stack[2])
